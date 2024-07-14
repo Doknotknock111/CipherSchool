9b06fbc8a4fad2f1c7f66583a5a72890e4ddb4c3
Doknotknock111 <126953822+Doknotknock111@users.noreply.github.com>
2024-07-14 18:29:54 +0530

    Add files via upload

---
+++ CipherSchool/Week 1/Javascript/Lec15/example.js
@@ -0,0 +1,54 @@
+console.log("The console statement!!!");
+
+// var score= 10;
+// let res = 15;
+// const PI = 3.14;
+
+// console.log(score);
+// console.log(res);
+// console.log(PI);
+
+// let sc= 10;
+// let final = '10';
+// let finalsc = sc+final; // concatenates
+// let minus = sc - final; // performs mathematical operation
+// let totalgame = 'five';
+// console.log(finalsc, minus);
+// console.log(typeof finalsc);
+// console.log(typeof minus);
+// //BEDMAS
+// let value = (sc ** 2) *10 /(minus + totalgame); // NaN: Not a number
+// console.log(value);
+
+console.log("it's raining");
+// console.log('it's raining today);
+
+let word ="raining";
+console.log(word.toUpperCase());
+console.log(word.charAt(2));
+console.log('Lenght: ' +word.length);
+
+console.log(typeof true);
+console.log(isNaN('five'));
+console.log(typeof isNaN('five'));
+if('1'){ //true 1 
+    console.log('inside if block');
+}
+else{
+    console.log('inside else block');
+}
+
+if(''){ // ' ' empty string, false , 0
+    console.log('inside if block');
+}
+else{
+    console.log('inside else block');
+}
+
+console.log('hello'.includes('e'));
+
+let score;
+let res = null;
+console.log(typeof score);
+console.log(typeof res);
+    
