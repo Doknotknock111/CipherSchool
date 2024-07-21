d643366adf71e4e42e6c768e50a02017de4c69a2
Doknotknock111 <126953822+Doknotknock111@users.noreply.github.com>
2024-07-21 21:25:59 +0530

    Delete Week 2/Node/Lec 23 directory

--- Week 2/Node/Lec 23/ClientandServer/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-let server = http.createServer((request, response) => {
-    response.end('Hello');
-});
-
-server.listen(5000, 'localhost', () => {
-    console.log("Started the server... and listening at port 5000");
-});
